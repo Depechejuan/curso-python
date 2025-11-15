01d58cbf4853d63e7dbbe9f414aab804ee82f119
Juan León <depechejuan@gmail.com>
2025-11-15 18:48:15 +0100

    creacion y eliminacion de clases en bbdd

--- Django/modularizacion/comentarios/views.py
+++ Django/modularizacion/comentarios/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Comment
 
 # Create your views here.
+def test(req):
+    return HttpResponse("funciona correctamente")
+
+def create(req):
+    # comment = Comment(name="Juanjo", score=5, comment="Esto es un comentario")
+    # comment.save()
+
+    # comment = Comment.objects.create(name="Alex")
+    return HttpResponse("TODO")
+
+def delete(req):
+    # comment = Comment.objects.get(id=1)
+    # comment.delete()
+    # Comment.objects.filter(id=2).delete()
+    return HttpResponse("Usuario borrado")
